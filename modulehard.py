d362d8f0c219c2c5238d3b77fd2cc1ae242fa56e
pinkklss <shaker_04@mail.ru>
2024-05-30 19:53:26 +0700

    Add files via upload

---
+++ modulehard.py
@@ -0,0 +1,13 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khenderik', 'Aaron'}
+print(sorted(students))
+
+a = sum(grades[0]) / len(grades[0])
+b = sum(grades[1]) / len(grades[1])
+c = sum(grades[2]) / len(grades[2])
+d = sum(grades[3]) / len(grades[3])
+f = sum(grades[4]) / len(grades[4])
+mean = dict([['Aaron', a] ,['Bilbo',]] )
+
+print(mean)
+
