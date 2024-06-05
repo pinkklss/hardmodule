eefcc7c774da096784119faa2eb9c7bf626ccc2e
pinkklss <shaker_04@mail.ru>
2024-06-05 15:23:32 +0700

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -1,9 +1,9 @@
 n = int(input('Введите число от 3 до 20: '))
 result = ''
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if(i+j) % n == 0:
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if (i + j) % n == 0:
             result = str(i + j)
 
 print(result)
